f52945e2a1cea90c0f0cbfdfe2b0a3428415e692
Jesse Beach <splendidnoise@gmail.com>
2011-06-26 13:57:21 -0400

    added the hide and show methods so that the function doesn't simply toggle visibility.
    Signed-off-by: Jesse Beach <splendidnoise@gmail.com>

--- jquery.smartToggle.js
+++ jquery.smartToggle.js
@@ -1,53 +1,81 @@
-(function($) {
-
-  // replace 'smartToggle' with the name of your plugin
-  $.fn.smartToggle = function(action, options) {
-
-    // debug(this);
-
-    // build main options before element iteration
-    var a = (options && (typeof action === "string")) ? action : null;
-    var options = (action && (typeof options === "object")) ? options : action;
-    // merge options with the defaults
-    var opts = $.extend({}, $.fn.smartToggle.defaults, options);
-
-    // iterate over matched elements
-    return this.each(function() {
-      var $this = $(this);
-      // build element specific options. Uses the Metadata plugin if available
-      // @see http://docs.jquery.com/Plugins/Metadata/metadata
-      var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-      // implementations
-      var visible = (!$this.hasClass('smart-hidden'));
-      if (visible) {
-        $this.fadeOut(o.speed).addClass('smart-hidden').show(0);
+(function ($) {
+  // Plugins should not declare more than one namespace in the $.fn object.
+  // So we declare methods in a methods array
+    var methods = {
+      init : function (options) {
+        // build main options before element iteration
+        var o = mergeOptions(options);
+        // iterate over matched elements
+        return this.each(function () {
+          // implementations
+          var visible = (!$(this).hasClass('smart-hidden'));
+          if (visible) {
+            hide(this, o)
+          }
+          else {
+            show(this, o);
+          }
+        });
+      },
+      show : function (options) {
+        // build main options before element iteration
+        var o = mergeOptions(options);
+        // iterate over matched elements
+        return this.each(function () {
+          show(this, o);
+        });
+      },
+      hide : function (options) {
+        // build main options before element iteration
+        var o = mergeOptions(options);
+        // iterate over matched elements
+        return this.each(function () {
+          hide(this, o);
+        });
       }
-      else {
-        $this.hide(0).removeClass('smart-hidden').fadeIn(o.speed);
-      }
-    });
-  };
-    
-  // plugin defaults
-  $.fn.smartToggle.defaults = {
-    action: null,
-    vClass: "smart-visible",
-    hClass: "smart-hidden",
-    speed: 750
-  };
+    };
 
-  // public functions definition
-  $.fn.smartToggle.functionName = function(foo) {
-    return this;
-  };
+    // replace 'smartToggle' with the name of your plugin
+    $.fn.smartToggle = function (method) {
   
-  // private functions definition
-  function foobar() {}
-
-  // private function for debugging
-  function debug() {
-    if (window.console && window.console.log)
-      window.console.log('selection count: ' + $(this).size());
-  };
-
-})(jQuery);
+      // debug(this);
+      
+      // Method calling logic
+      if (methods[method]) {
+        return methods[method].apply(this, Array.prototype.slice.call(arguments, 1));
+      } else if (typeof method === 'object' || ! method) {
+        return methods.init.apply(this, arguments);
+      } else {
+        $.error('Method ' +  method + ' does not exist on jQuery.smartToggle');
+      }
+    };
+    
+    // plugin defaults
+    $.fn.smartToggle.defaults = {
+      vClass: "smart-visible",
+      hClass: "smart-hidden",
+      speed: 750
+    };
+    
+    // private functions definition
+    function mergeOptions (options) {
+      return $.extend({}, $.fn.smartToggle.defaults, options);
+    }
+    function show (element, options) {
+      var $this = $(element);
+      $this.hide(0).removeClass('smart-hidden').fadeIn(options.speed);
+    }
+    function hide (element, options) {
+      var $this = $(element);
+      $this.fadeOut(options.speed).addClass('smart-hidden').show(0);
+    }
+  
+    // private function for debugging
+    function debug() {
+      var $this = $(this);
+      if (window.console && window.console.log) {
+        window.console.log('selection count: ' + $this.size());
+      }
+    }
+  }
+)(jQuery);
